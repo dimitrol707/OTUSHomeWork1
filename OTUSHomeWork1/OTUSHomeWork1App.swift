4aedd4a8e7a20ae9ea4793af75e1cc2bd3f3b003
DIM <dimitrol7171@gmail.com>
2021-12-05 10:13:31 +0300

    Initial Commit

---
+++ OTUSHomeWork1/OTUSHomeWork1App.swift
@@ -0,0 +1,17 @@
+//
+//  OTUSHomeWork1App.swift
+//  OTUSHomeWork1
+//
+//  Created by Дмитрий Мальцев on 05.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct OTUSHomeWork1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
